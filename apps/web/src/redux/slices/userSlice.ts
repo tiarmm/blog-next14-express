1c89d51865f459973568e32d03685cfaf1aa3497
tiarmm <septiana.rapihmaharani5@gmail.com>
2024-04-30 11:43:37 +0700

    chore: initial setup

---
+++ apps/web/src/redux/slices/userSlice.ts
@@ -0,0 +1,35 @@
+import type { PayloadAction } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+
+interface UserState {
+    id: number,
+    fullName: string,
+    email: string,
+}
+
+const initialState: UserState = {
+  id: 0,
+  fullName: "",
+  email: "",
+};
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    loginAction: (state, action: PayloadAction<UserState>) => {
+      state.id = action.payload.id;
+      state.fullName = action.payload.fullName;
+      state.email = action.payload.email;
+    },
+    logoutAction: (state) => {
+      state.id = 0;
+      state.fullName = "";
+      state.email = "";
+    },
+  },
+});
+
+export const { loginAction, logoutAction } = userSlice.actions;
+
+export default userSlice.reducer;
